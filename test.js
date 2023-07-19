539423dfcfc3b944525f4bd116fefbe76c43d5bb
andriano812 <96529887+andriano812@users.noreply.github.com>
2023-07-19 12:05:51 +0300

    Проверка автотестов

---
+++ test.js
@@ -0,0 +1,4 @@
+let dialog_window = document.querySelector('.modal-window');
+let dialog_background = document.querySelector('.modal-background');
+dialog_background.classList.add('show')
+dialog_window.show()
